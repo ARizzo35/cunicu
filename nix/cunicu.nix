49fe336ad3b8dc8a819f47c34860c5ca08f96867
Adam Rizkalla <ajarizzo@gmail.com>
2025-03-13 23:38:25 +0000

    test: fix buildGoModule

--- nix/cunicu.nix
+++ nix/cunicu.nix
@@ -4,7 +4,7 @@
 {
   lib,
   stdenv,
-  buildGo124Module,
+  buildGoModule,
   installShellFiles,
   versionCheckHook,
   protobuf,
@@ -16,7 +16,7 @@ let
   version = "0.12.1";
   src = ./..;
 in
-buildGo124Module {
+buildGoModule {
   pname = "cunicu";
   inherit version src;
 
